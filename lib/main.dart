1db566c31de9eed51d70586247db4035f8755923
Yoon Joo <yoonjoo3615@gmail.com>
2022-03-21 12:32:45 +0100

    Android Throw error  : No Firebase App '[DEFAULT]' has been created. To fixed this added initializeApp function on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
