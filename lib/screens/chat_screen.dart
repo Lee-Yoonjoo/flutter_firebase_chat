1a7be3d1d54991ee714dbecceaf29476fafeecd6
Yoon Joo <yoonjoo3615@gmail.com>
2022-03-21 16:06:16 +0100

    Display data on a list read from firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,25 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
+  bool isConnected = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('Chat Item view'),
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('chats/NwC82NLL2WPoxO48FGZc/messages')
+            .snapshots(),
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            isConnected = false;
+          } else {
+            isConnected = true;
+          }
+          final documents = streamSnapshot.data?.docs;
+          return isConnected
+              ? ListView.builder(
+                  itemCount: streamSnapshot.data?.docs.length,
+                  itemBuilder: (context, index) => Container(
+                    padding: EdgeInsets.all(8),
+                    child: Text(documents![index]['text']),
+                  ),
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          FirebaseFirestore.instance
+          /*         FirebaseFirestore.instance
               .collection('chats/NwC82NLL2WPoxO48FGZc/messages')
               .snapshots()
               .listen((event) {
-                print(event.docs[0]['text']);
-          });
+            event.docs.forEach((element) {
+              print('ForEach ' + element['text']);
+            });
+          });*/
         },
       ),
     );
